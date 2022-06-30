683c26620667f2c4249795a561ed44601251dd0f
kree1 <myopicmastermind@gmail.com>
2022-06-30 13:00:41 -0400

    Added MrWolf and TheCloisteredPrince CardControllers

---
+++ MyMod/TheMidnightBazaar/MrWolfCardController.cs
@@ -0,0 +1,56 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacade.TheMidnightBazaar
+{
+    public class MrWolfCardController : TheMidnightBazaarUtilityCardController
+    {
+        public MrWolfCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show whether The Blinded Queen is in play
+            SpecialStringMaker.ShowIfSpecificCardIsInPlay(BlindedQueenIdentifier);
+            // Show target with lowest HP
+            SpecialStringMaker.ShowLowestHP(1, () => 1);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "When this card would deal damage, if [i]The Blinded Queen[/i] is in play, play the top card of the villain deck instead."
+            AddTrigger((DealDamageAction dda) => dda.DamageSource != null && dda.DamageSource.Card == base.Card && IsBlindedQueenInPlay(), PlayVillainCardInsteadResponse, new TriggerType[]{ TriggerType.WouldBeDealtDamage, TriggerType.CancelAction, TriggerType.PlayCard}, TriggerTiming.Before);
+            // "At the end of the environment turn, this card deals the target with the lowest HP {H + 1} melee damage."
+            AddDealDamageAtEndOfTurnTrigger(TurnTaker, base.Card, (Card c) => c.IsTarget, TargetType.LowestHP, H + 1, DamageType.Melee);
+        }
+
+        private IEnumerator PlayVillainCardInsteadResponse(DealDamageAction dda)
+        {
+            // "... play the top card of the villain deck instead."
+            IEnumerator cancelCoroutine = CancelAction(dda);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(cancelCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(cancelCoroutine);
+            }
+            IEnumerator playCoroutine = PlayTheTopCardOfTheVillainDeckWithMessageResponse(dda);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(playCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(playCoroutine);
+            }
+            yield break;
+        }
+    }
+}
---
+++ MyMod/TheMidnightBazaar/TheCloisteredPrinceCardController.cs
@@ -0,0 +1,94 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacade.TheMidnightBazaar
+{
+    public class TheCloisteredPrinceCardController : TheMidnightBazaarUtilityCardController
+    {
+        public TheCloisteredPrinceCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator DeterminePlayLocation(List<MoveCardDestination> storedResults, bool isPutIntoPlay, List<IDecision> decisionSources, Location overridePlayArea = null, LinqTurnTakerCriteria additionalTurnTakerCriteria = null)
+        {
+            // "Play this card in a hero play area."
+            List<SelectTurnTakerDecision> storedDecisions = new List<SelectTurnTakerDecision>();
+            IEnumerator selectCoroutine = base.GameController.SelectHeroTurnTaker(DecisionMaker, SelectionType.MoveCardToPlayArea, optional: false, allowAutoDecide: false, storedDecisions, null, null, allowIncapacitatedHeroes: false, null, null, canBeCancelled: true, null, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(selectCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(selectCoroutine);
+            }
+            SelectTurnTakerDecision selection = storedDecisions.FirstOrDefault();
+            if (selection != null && selection.SelectedTurnTaker != null)
+            {
+                storedResults?.Add(new MoveCardDestination(selection.SelectedTurnTaker.PlayArea));
+            }
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "Increase damage dealt to and by targets in this play area by 1."
+            AddIncreaseDamageTrigger((DealDamageAction dda) => dda.Target.Location.HighestRecursiveLocation == base.Card.Location.HighestRecursiveLocation, (DealDamageAction dda) => 1);
+            AddIncreaseDamageTrigger((DealDamageAction dda) => dda.DamageSource != null && dda.DamageSource.Card.IsTarget && dda.DamageSource.Card.Location.HighestRecursiveLocation == base.Card.Location.HighestRecursiveLocation, (DealDamageAction dda) => 1);
+            // "At the end of that hero's turn, they may play a card or use a power. If they do, play the top card of the environment or villain deck."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker, PlayOrPowerWithCostResponse, new TriggerType[] { TriggerType.PlayCard, TriggerType.UsePower });
+        }
+
+        private IEnumerator PlayOrPowerWithCostResponse(PhaseChangeAction pca)
+        {
+            // "... they may play a card or use a power."
+            List<Function> options = new List<Function>();
+            List<PlayCardAction> playedCards = new List<PlayCardAction>();
+            List<UsePowerDecision> usedPowers = new List<UsePowerDecision>();
+            options.Add(new Function(base.GameController.FindTurnTakerController(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker).ToHero(), "Play a card", SelectionType.PlayCard, () => SelectAndPlayCardFromHand(base.GameController.FindTurnTakerController(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker).ToHero(), storedResults: playedCards)));
+            options.Add(new Function(base.GameController.FindTurnTakerController(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker).ToHero(), "Use a power", SelectionType.UsePower, () => base.GameController.SelectAndUsePower(base.GameController.FindTurnTakerController(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker).ToHero(), storedResults: usedPowers, cardSource: GetCardSource())));
+            SelectFunctionDecision select = new SelectFunctionDecision(GameController, base.GameController.FindTurnTakerController(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker).ToHero(), options, true, noSelectableFunctionMessage: base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name + " cannot play cards or use powers.", cardSource: GetCardSource());
+            IEnumerator selectCoroutine = base.GameController.SelectAndPerformFunction(select);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(selectCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(selectCoroutine);
+            }
+            // "If they do, play the top card of the environment or villain deck."
+            bool didSomething = false;
+            if (playedCards.Any((PlayCardAction play) => play.WasCardPlayed))
+                didSomething = true;
+            UsePowerDecision used = usedPowers.FirstOrDefault();
+            if (used != null && used.SelectedPower != null)
+                didSomething = true;
+            if (didSomething)
+            {
+                List<Function> consequences = new List<Function>();
+                consequences.Add(new Function(DecisionMaker, "Play the top card of the environment deck", SelectionType.PlayTopCardOfEnvironmentDeck, () => PlayTheTopCardOfTheEnvironmentDeckWithMessageResponse(null), FindEnvironment().TurnTaker.Deck.HasCards || FindEnvironment().TurnTaker.Trash.HasCards));
+                consequences.Add(new Function(DecisionMaker, "Play the top card of the villain deck", SelectionType.PlayTopCardOfVillainDeck, () => PlayTheTopCardOfTheVillainDeckWithMessageResponse(null), FindVillainTurnTakerControllers(true).Any((TurnTakerController ttc) => ttc.TurnTaker.Deck.HasCards || ttc.TurnTaker.Trash.HasCards)));
+                SelectFunctionDecision poison = new SelectFunctionDecision(GameController, DecisionMaker, consequences, false, cardSource: GetCardSource());
+                IEnumerator pickCoroutine = base.GameController.SelectAndPerformFunction(poison);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(pickCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(pickCoroutine);
+                }
+            }
+            yield break;
+        }
+    }
+}
