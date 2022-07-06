486c7a2988496cf7ca290ccc9ade830e41f6b287
kree1 <myopicmastermind@gmail.com>
2022-07-06 12:07:02 -0400

    Added post-build event to automatically copy DLL, manifest.json, preview.jpg, and Resources subfolders to local mod folder

---
+++ MyMod/postbuild.bat
@@ -0,0 +1,11 @@
+copy /y "C:\Users\Kendal\source\repos\VainFacade\MyMod\bin\Release\VainFacade.dll" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade"
+copy /y "C:\Users\Kendal\source\repos\VainFacade\Resources\manifest.json" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade"
+copy /y "C:\Users\Kendal\source\repos\VainFacade\Resources\preview.png" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade"
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\Atlas" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\Atlas" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\Cutouts" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\DeckBrowser" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\DeckBrowser" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\Endings" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\Endings" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\Environments" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\Environments" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\Fonts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\Fonts" /e
+robocopy "C:\Users\Kendal\source\repos\VainFacade\Resources\LargeCardTextures" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacade\LargeCardTextures" /e
+exit 0
