83996bcb2c5bba598e9dc355b6be110b31533ab1
kree1 <myopicmastermind@gmail.com>
2022-07-06 11:45:49 -0400

    Changed The Poet of the Pale to only activate "cards aren't indestructible anymore, clean 'em up" if it's not already mid-activation

--- MyMod/TheMidnightBazaar/ThePoetOfThePaleCardController.cs
+++ MyMod/TheMidnightBazaar/ThePoetOfThePaleCardController.cs
@@ -22,12 +22,14 @@ namespace VainFacade.TheMidnightBazaar
         public override bool AskIfCardIsIndestructible(Card card)
         {
             // "Threens are indestructible if any Threen has 1 or more HP."
-            if (FindCardsWhere(new LinqCardCriteria((Card c) => IsThreen(c) && c.IsInPlayAndHasGameText && c.IsTarget && c.HitPoints >= 1)).Any())
+            if (FindCardsWhere(new LinqCardCriteria((Card c) => IsThreen(c) && c.IsInPlayAndHasGameText && c.IsTarget && c.HitPoints >= 1 && base.GameController.IsCardVisibleToCardSource(c, GetCardSource()))).Any())
             {
-                return IsThreen(card);
+                return IsThreen(card) && base.GameController.IsCardVisibleToCardSource(card, GetCardSource());
             }
             return false;
         }
+
+        protected const string IsCleaningUp = "IsCleaningUp";
 
         public override void AddTriggers()
         {
@@ -35,11 +37,35 @@ namespace VainFacade.TheMidnightBazaar
             // "At the end of the environment turn, this card deals the non-Threen target with the lowest HP {H - 2} psychic damage."
             AddDealDamageAtEndOfTurnTrigger(base.TurnTaker, base.Card, (Card c) => !IsThreen(c), TargetType.LowestHP, H - 2, DamageType.Psychic);
             // When the last Threen with >0 HP leaves play, other Threen lose indestructibility
-            AddAfterLeavesPlayAction((GameAction g) => base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: false, GetCardSource()), TriggerType.DestroyCard);
-            AddTrigger((DestroyCardAction dca) => IsThreen(dca.CardToDestroy.Card), (DestroyCardAction dca) => base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: true, GetCardSource()), TriggerType.DestroyCard, TriggerTiming.After);
-            AddTrigger((BulkMoveCardsAction bmc) => bmc.CardsToMove.Any((Card c) => IsThreen(c)) && bmc.Destination.IsOutOfGame, (BulkMoveCardsAction bmc) => base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: true, GetCardSource()), TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: true);
-            AddTrigger((SwitchBattleZoneAction sb) => sb.Origin == base.Card.BattleZone, (SwitchBattleZoneAction sb) => base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: true, GetCardSource()), TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: false, null, null, ignoreBattleZone: true);
-            AddTrigger((MoveCardAction mc) => mc.Origin.BattleZone == base.BattleZone && mc.Destination.BattleZone != base.BattleZone, (MoveCardAction mc) => base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: true, GetCardSource()), TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: false, null, null, ignoreBattleZone: true);
+            AddAfterLeavesPlayAction(CleanUpThreens, TriggerType.DestroyCard);
+            AddTrigger((DestroyCardAction dca) => IsThreen(dca.CardToDestroy.Card), CleanUpThreens, TriggerType.DestroyCard, TriggerTiming.After);
+            AddTrigger((BulkMoveCardsAction bmc) => bmc.CardsToMove.Any((Card c) => IsThreen(c)) && bmc.Destination.IsOutOfGame, CleanUpThreens, TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: true);
+            AddTrigger((SwitchBattleZoneAction sb) => sb.Origin == base.Card.BattleZone, CleanUpThreens, TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: false, null, null, ignoreBattleZone: true);
+            AddTrigger((MoveCardAction mc) => mc.Origin.BattleZone == base.BattleZone && mc.Destination.BattleZone != base.BattleZone, CleanUpThreens, TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, isConditional: false, requireActionSuccess: true, null, outOfPlayTrigger: false, null, null, ignoreBattleZone: true);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(IsCleaningUp), TriggerType.Hidden);
+        }
+
+        private IEnumerator CleanUpThreens(GameAction ga)
+        {
+            // When the last Threen with >0 HP leaves play, other Threen lose indestructibility and need cleaning up
+            if (Journal.GetCardPropertiesBoolean(base.Card, IsCleaningUp).HasValue && Journal.GetCardPropertiesBoolean(base.Card, IsCleaningUp).Value)
+            {
+                // We don't need to start this process again if it's already running
+                yield break;
+            }
+            if (IsRealAction())
+            {
+                Journal.RecordCardProperties(base.Card, IsCleaningUp, true);
+            }
+            IEnumerator cleanupCoroutine = base.GameController.DestroyAnyCardsThatShouldBeDestroyed(ignoreBattleZone: true, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(cleanupCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(cleanupCoroutine);
+            }
         }
     }
 }
