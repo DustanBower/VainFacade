d58902353c904b73347de53939d6d84f1ce72bfd
Dustan Bower <dustan.bower@gmail.com>
2022-07-14 10:14:19 -0400

    fix preview image name in postinstall

--- VainFacade/postbuild.bat
+++ VainFacade/postbuild.bat
@@ -1,6 +1,6 @@
 copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\VainFacade\bin\Release\VainFacadePlaytest.dll" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
 copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\manifest.json" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
-copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\preview.png" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
+copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\preview.jpg" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Atlas" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Atlas" /e
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Art\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Cutouts" /e
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\DeckBrowser" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\DeckBrowser" /e
