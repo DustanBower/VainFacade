730f5dc55faa65595db8033e1fb46f9d1cfdbf9a
kree1 <myopicmastermind@gmail.com>
2022-07-21 13:30:52 -0400

    Streamlined Emanation start of turn effect if all Emanations in play match; added custom decision text if they don't

--- VainFacade/Sphere/EmanationCardController.cs
+++ VainFacade/Sphere/EmanationCardController.cs
@@ -28,22 +28,34 @@ namespace VainFacadePlaytest.Sphere
         protected IEnumerator FocusEmanations(PhaseChangeAction pca)
         {
             // "... select an Emanation. Destroy all Emanations that are not copies of that card."
-            List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
-            IEnumerator selectCoroutine = base.GameController.SelectCardAndStoreResults(base.HeroTurnTakerController, SelectionType.Custom, SphereUtilityCardController.isEmanationInPlay, storedResults, false, cardSource: GetCardSource());
-            if (base.UseUnityCoroutines)
+            // If no Emanation is chosen, all Emanations will be destroyed
+            LinqCardCriteria toDestroy = SphereUtilityCardController.isEmanationInPlay;
+            List<Card> allEmanations = base.GameController.FindCardsWhere(isEmanationInPlay, visibleToCard: GetCardSource()).ToList();
+            if (allEmanations.Where((Card c) => c.Title != base.Card.Title || c.Owner != base.Card.Owner).Any())
             {
-                yield return base.GameController.StartCoroutine(selectCoroutine);
+                // If there are multiple different Emanations in play, the player chooses one
+                List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+                IEnumerator selectCoroutine = base.GameController.SelectCardAndStoreResults(base.HeroTurnTakerController, SelectionType.Custom, SphereUtilityCardController.isEmanationInPlay, storedResults, false, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(selectCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(selectCoroutine);
+                }
+                SelectCardDecision choice = storedResults.FirstOrDefault();
+                if (choice != null && choice.SelectedCard != null)
+                {
+                    toDestroy = new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain(emanationKeyword) && c.Title != choice.SelectedCard.Title, "in play other than copies of " + choice.SelectedCard.Title, false, true, "Emanation", "Emanations");
+                }
             }
             else
             {
-                base.GameController.ExhaustCoroutine(selectCoroutine);
+                // If all Emanations in play have the same title and deck identity, the player doesn't need to choose- nothing will be destroyed because there are no non-matching Emanations
+                toDestroy = new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain(emanationKeyword) && (c.Title != base.Card.Title || c.Owner != base.Card.Owner), "in play other than copies of " + base.Card.Title, false, true, "Emanation", "Emanations");
             }
-            SelectCardDecision choice = storedResults.FirstOrDefault();
-            LinqCardCriteria toDestroy = SphereUtilityCardController.isEmanationInPlay;
-            if (choice != null && choice.SelectedCard != null)
-            {
-                toDestroy = new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain(emanationKeyword) && c.Title != choice.SelectedCard.Title, " in play other than copies of " + choice.SelectedCard.Title, false, false, "Emanation", "Emanations");
-            }
+            // ...
             IEnumerator destroyCoroutine = base.GameController.DestroyCards(base.HeroTurnTakerController, toDestroy, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
@@ -54,6 +66,11 @@ namespace VainFacadePlaytest.Sphere
                 base.GameController.ExhaustCoroutine(destroyCoroutine);
             }
             yield break;
+        }
+
+        public override CustomDecisionText GetCustomDecisionText(IDecision decision)
+        {
+            return new CustomDecisionText("Select an Emanation to maintain", "choosing an Emanation to maintain", "Vote for an Emanation for " + base.TurnTaker.Name + " to maintain", "Emanation to maintain");
         }
     }
 }
