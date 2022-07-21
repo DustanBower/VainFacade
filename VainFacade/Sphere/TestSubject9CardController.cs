68364d7c077bd223556793bc41d846c3c03bd6a0
kree1 <myopicmastermind@gmail.com>
2022-07-21 11:09:14 -0400

    Added "reveal until" CardControllers

---
+++ VainFacade/Sphere/TestSubject9CardController.cs
@@ -0,0 +1,71 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Sphere
+{
+    public class TestSubject9CardController : SphereUtilityCardController
+    {
+        public TestSubject9CardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show number of Emanations in Sphere's deck
+            SpecialStringMaker.ShowNumberOfCardsAtLocation(base.TurnTaker.Deck, isEmanation);
+        }
+
+        public override IEnumerator Play()
+        {
+            // "Reveal cards from the top of your deck until 2 Emanations are revealed."
+            List<RevealCardsAction> results = new List<RevealCardsAction>();
+            IEnumerator revealCoroutine = base.GameController.RevealCards(base.TurnTakerController, base.TurnTaker.Deck, (Card c) => c.DoKeywordsContain(emanationKeyword), 2, results, RevealedCardDisplay.Message, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(revealCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(revealCoroutine);
+            }
+            // "Put one into play..."
+            List<Card> revealedCards = results.SelectMany((RevealCardsAction rca) => rca.RevealedCards).ToList();
+            LinqCardCriteria wasRevealedEmanation = new LinqCardCriteria((Card c) => c.DoKeywordsContain(emanationKeyword) && revealedCards.Contains(c));
+            IEnumerator playCoroutine = base.GameController.SelectCardFromLocationAndMoveIt(base.HeroTurnTakerController, base.TurnTaker.Revealed, wasRevealedEmanation, new MoveCardDestination[] { new MoveCardDestination(base.TurnTaker.PlayArea) }, isPutIntoPlay: true, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(playCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(playCoroutine);
+            }
+            // "... and one into your hand."
+            IEnumerator handCoroutine = base.GameController.SelectCardFromLocationAndMoveIt(base.HeroTurnTakerController, base.TurnTaker.Revealed, wasRevealedEmanation, new MoveCardDestination[] { new MoveCardDestination(base.HeroTurnTaker.Hand) }, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(handCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(handCoroutine);
+            }
+            // "Discard the other revealed cards."
+            List<Location> toClean = new List<Location>();
+            toClean.Add(base.TurnTaker.Revealed);
+            IEnumerator cleanupCoroutine = CleanupCardsAtLocations(toClean, base.TurnTaker.Trash, isDiscard: true, isReturnedToOriginalLocation: false, cardsInList: revealedCards);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(cleanupCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(cleanupCoroutine);
+            }
+            yield break;
+        }
+    }
+}
---
+++ VainFacade/Sphere/X51QuantumFieldStabilizerCardController.cs
@@ -0,0 +1,36 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Sphere
+{
+    public class X51QuantumFieldStabilizerCardController : SphereUtilityCardController
+    {
+        public X51QuantumFieldStabilizerCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show number of Emanations in Sphere's deck
+            SpecialStringMaker.ShowNumberOfCardsAtLocation(base.TurnTaker.Deck, isEmanation);
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            // "Reveal cards from the top of your deck until an Emanation is revealed. Put it into your hand or into play. Discard the other revealed cards."
+            IEnumerator moveCoroutine = RevealCards_SelectSome_MoveThem_DiscardTheRest(base.HeroTurnTakerController, base.TurnTakerController, base.TurnTaker.Deck, (Card c) => c.DoKeywordsContain(emanationKeyword), 1, 1, true, true, true, emanationKeyword, base.TurnTaker);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(moveCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(moveCoroutine);
+            }
+            yield break;
+        }
+    }
+}
