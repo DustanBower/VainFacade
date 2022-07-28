7b510011284c8330526192b7e7c91dff28114a62
kree1 <myopicmastermind@gmail.com>
2022-07-27 23:50:03 -0400

    Fixed Cutouts source location in postbuild.bat

--- VainFacade/postbuild.bat
+++ VainFacade/postbuild.bat
@@ -2,7 +2,7 @@ copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\VainFacade\bin\Release\Vain
 copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\manifest.json" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
 copy /y "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\preview.jpg" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest"
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Atlas" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Atlas" /e
-robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Art\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Cutouts" /e
+robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Cutouts" /e
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\DeckBrowser" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\DeckBrowser" /e
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Art\Endings" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Endings" /e
 robocopy "C:\Users\%USERNAME%\source\repos\VainFacade\Resources\Art\Environments" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\VainFacadePlaytest\Environments" /e
