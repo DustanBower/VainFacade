4d4d28bcc42cfc32d5a85152e88243cf6d09bc35
kree1 <myopicmastermind@gmail.com>
2022-08-01 15:48:18 -0400

    Added TheBaronessCharacterCardController

---
+++ VainFacade/TheBaroness/BaronessUtilityCardController.cs
@@ -0,0 +1,35 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.TheBaroness
+{
+    public class BaronessUtilityCardController : CardController
+    {
+        public BaronessUtilityCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+        }
+
+        public override IEnumerator Play()
+        {
+            yield break;
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            yield break;
+        }
+    }
+}
