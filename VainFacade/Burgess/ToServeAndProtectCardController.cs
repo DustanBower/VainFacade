17ac09c7792ef1ee6cbd67ee167dceac8154720f
kree1 <myopicmastermind@gmail.com>
2022-08-14 15:28:44 -0400

    Made sure To Serve and Protect checks for redirection since the start of Burgess's last turn, not since the end

--- VainFacade/Burgess/ToServeAndProtectCardController.cs
+++ VainFacade/Burgess/ToServeAndProtectCardController.cs
@@ -26,7 +26,46 @@ namespace VainFacadePlaytest.Burgess
             // "When a target in this play area would take damage, you may redirect that damage to a target in your play area."
             AddTrigger((DealDamageAction dda) => dda.Target.IsAtLocationRecursive(base.Card.Location), (DealDamageAction dda) => base.GameController.SelectTargetAndRedirectDamage(base.HeroTurnTakerController, (Card c) => c.IsTarget && c.IsAtLocationRecursive(base.TurnTaker.PlayArea), dda, optional: true, cardSource: GetCardSource()), new TriggerType[] { TriggerType.WouldBeDealtDamage, TriggerType.RedirectDamage }, TriggerTiming.Before);
             // "At the start of your turn, if you redirected damage this way since the start of your last turn, discard 3 cards or destroy this card."
-            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker && base.GameController.Game.Journal.QueryJournalEntries((RedirectDamageJournalEntry r) => r.CardSource == base.Card && r.AmoutRedirected > 0).Where(base.GameController.Game.Journal.SinceLastTurn<RedirectDamageJournalEntry>(base.TurnTaker)).Any(), DiscardOrSelfDestructResponse, new TriggerType[] { TriggerType.DiscardCard, TriggerType.DestroySelf });
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker && base.GameController.Game.Journal.QueryJournalEntries((RedirectDamageJournalEntry r) => r.CardSource == base.Card && r.AmoutRedirected > 0).Where(SinceStartOfLastTurn<RedirectDamageJournalEntry>(base.TurnTaker)).Where(base.GameController.Game.Journal.SinceCardWasPlayed<RedirectDamageJournalEntry>(base.Card)).Any(), DiscardOrSelfDestructResponse, new TriggerType[] { TriggerType.DiscardCard, TriggerType.DestroySelf });
+        }
+
+        public Func<T, bool> SinceStartOfLastTurn<T>(TurnTaker turnTaker) where T : JournalEntry
+        {
+            if ((from pcje in base.GameController.Game.Journal.PhaseChangeEntries()
+                 where pcje.ToPhase != null && pcje.ToPhase.TurnTaker == turnTaker && pcje.ToPhase.IsStart
+                 select pcje).Any())
+            {
+                int? turnTakerIndex = Game.TurnTakers.IndexOf(turnTaker);
+                if (turnTakerIndex.HasValue)
+                {
+                    return delegate (T e)
+                    {
+                        int num = 0;
+                        if (e.TurnPhase != null)
+                        {
+                            num = Game.TurnTakers.IndexOf(e.TurnPhase.TurnTaker).GetValueOrDefault(0);
+                        }
+                        int num2 = 0;
+                        if (Game.ActiveTurnPhase != null)
+                        {
+                            num2 = Game.TurnTakers.IndexOf(Game.ActiveTurnPhase.TurnTaker).GetValueOrDefault(0);
+                        }
+                        int round = e.Round;
+                        int round2 = Game.Round;
+                        if (num2 > turnTakerIndex)
+                        {
+                            if (round == round2)
+                            {
+                                return num > turnTakerIndex;
+                            }
+                            return false;
+                        }
+                        return (round == round2 - 1 && num > turnTakerIndex) || (round == round2 && num <= turnTakerIndex);
+                    };
+                }
+                return (T e) => false;
+            }
+            return (T e) => false;
         }
 
         public override IEnumerator DeterminePlayLocation(List<MoveCardDestination> destination, bool isPutIntoPlay, List<IDecision> decisionSources, Location overridePlayArea = null, LinqTurnTakerCriteria additionalTurnTakerCriteria = null)
