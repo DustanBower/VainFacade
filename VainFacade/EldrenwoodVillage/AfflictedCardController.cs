a2ec812ab588374db96b87e3e52233b6c0c7c270
kree1 <myopicmastermind@gmail.com>
2022-09-01 12:45:16 -0400

    Added SpecialStrings on Afflicted targets to indicate who they'll attack when they're Werewolves

--- VainFacade/EldrenwoodVillage/AfflictedCardController.cs
+++ VainFacade/EldrenwoodVillage/AfflictedCardController.cs
@@ -15,7 +15,26 @@ namespace VainFacadePlaytest.EldrenwoodVillage
             : base(card, turnTakerController)
         {
             // If Howls in the Distance is face up, remind the player that text on this card doesn't matter
-            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: Afflicted targets have no game text.").Condition = () => CanActivateEffect(base.TurnTakerController, HowlsKey);
+            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: " + base.Card.Title + " has transformed into a Werewolf!").Condition = () => base.GameController.DoesCardContainKeyword(base.Card, AfflictedKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: " + base.Card.Title + " has no game text.").Condition = () => base.GameController.DoesCardContainKeyword(base.Card, AfflictedKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            // If Howls in the Distance is face up and this is a Small Werewolf, indicate which target this card will attack
+            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: At the end of the environment turn, each Small Werewolf deals the non-Werewolf with the lowest HP " + (H - 2).ToString() + " irreducible melee damage.").Condition = () => base.GameController.DoesCardContainKeyword(base.Card, SmallKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            SpecialStringMaker.ShowLowestHP(1, cardCriteria: NonWerewolf()).Condition = () => base.GameController.DoesCardContainKeyword(base.Card, SmallKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            // If Howls in the Distance is face up and this is a Clever Werewolf, indicate which target this card will attack
+            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: At the end of the environment turn, each Clever Werewolf deals the non-Werewolf with the highest HP " + (H + 1).ToString() + " melee damage.").Condition = () => base.GameController.DoesCardContainKeyword(base.Card, CleverKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            SpecialStringMaker.ShowHighestHP(2, cardCriteria: NonWerewolf()).Condition = () => base.GameController.DoesCardContainKeyword(base.Card, CleverKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            // If Howls in the Distance is face up and this is a Common Werewolf, indicate which target this card will attack
+            SpecialStringMaker.ShowSpecialString(() => "Howls in the Distance: At the end of the environment turn, each Common Werewolf deals the non-Werewolf with the second lowest HP " + H.ToString() + " melee damage.").Condition = () => base.GameController.DoesCardContainKeyword(base.Card, CommonKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+            SpecialStringMaker.ShowLowestHP(2, cardCriteria: NonWerewolf()).Condition = () => base.GameController.DoesCardContainKeyword(base.Card, CommonKeyword) && base.GameController.DoesCardContainKeyword(base.Card, WerewolfKeyword) && CanActivateEffect(base.TurnTakerController, HowlsKey);
+        }
+
+        protected const string SmallKeyword = "small";
+        protected const string CleverKeyword = "clever";
+        protected const string CommonKeyword = "common";
+
+        protected LinqCardCriteria NonWerewolf()
+        {
+            return new LinqCardCriteria((Card c) => c.IsTarget && !base.GameController.DoesCardContainKeyword(c, WerewolfKeyword), "non-Werewolf");
         }
 
         public override IEnumerator ReducedToZeroResponse()
