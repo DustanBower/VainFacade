6ab1eec16293b390e328aa7a3f2ccd79f25858cc
kree1 <myopicmastermind@gmail.com>
2022-09-02 13:51:06 -0400

    Streamlined Tom Barton's move cards effect, made it correctly prevent real damage

--- VainFacade/EldrenwoodVillage/TomBartonCardController.cs
+++ VainFacade/EldrenwoodVillage/TomBartonCardController.cs
@@ -14,10 +14,13 @@ namespace VainFacadePlaytest.EldrenwoodVillage
         public TomBartonCardController(Card card, TurnTakerController turnTakerController)
             : base(card, turnTakerController)
         {
+            AllowFastCoroutinesDuringPretend = false;
             SpecialStringMaker.ShowHasBeenUsedThisTurn(TakenShelterThisTurn, base.Card.Title + " has already prevented damage dealt by a Werewolf this turn.", base.Card.Title + " has not prevented damage dealt by a Werewolf this turn.");
         }
 
         protected readonly string TakenShelterThisTurn = "TakenShelterThisTurn";
+
+        public Guid? MoveCardsForDamage { get; set; }
 
         public override void AddTriggers()
         {
@@ -29,22 +32,32 @@ namespace VainFacadePlaytest.EldrenwoodVillage
 
         private IEnumerator MoveCardsToPreventDamageResponse(DealDamageAction dda)
         {
-            YesNoDecision choice = new YesNoDecision(base.GameController, DecisionMaker, SelectionType.Custom, gameAction: dda, associatedCards: dda.Target.ToEnumerable(), cardSource: GetCardSource());
-            IEnumerator decideCoroutine = base.GameController.MakeDecisionAction(choice);
-            if (base.UseUnityCoroutines)
+            if (!MoveCardsForDamage.HasValue || MoveCardsForDamage.Value != dda.InstanceIdentifier)
             {
-                yield return base.GameController.StartCoroutine(decideCoroutine);
+                if (!HasBeenSetToTrueThisTurn(TakenShelterThisTurn))
+                {
+                    YesNoDecision choice = new YesNoDecision(base.GameController, DecisionMaker, SelectionType.Custom, gameAction: dda, associatedCards: dda.Target.ToEnumerable(), cardSource: GetCardSource());
+                    IEnumerator decideCoroutine = base.GameController.MakeDecisionAction(choice);
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(decideCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(decideCoroutine);
+                    }
+                    if (DidPlayerAnswerYes(choice))
+                    {
+                        MoveCardsForDamage = dda.InstanceIdentifier;
+                    }
+                }
             }
-            else
-            {
-                base.GameController.ExhaustCoroutine(decideCoroutine);
-            }
-            if (DidPlayerAnswerYes(choice))
+            if (MoveCardsForDamage.HasValue && MoveCardsForDamage.Value == dda.InstanceIdentifier && IsRealAction(dda))
             {
                 SetCardPropertyToTrueIfRealAction(TakenShelterThisTurn);
                 // "... each player may put a card in their hand on the bottom of their deck."
                 List<MoveCardAction> moved = new List<MoveCardAction>();
-                IEnumerator selectCoroutine = base.GameController.SelectTurnTakersAndDoAction(DecisionMaker, new LinqTurnTakerCriteria((TurnTaker tt) => tt.IsHero && !tt.ToHero().IsIncapacitatedOrOutOfGame && tt.ToHero().HasCardsInHand), SelectionType.MoveCardOnBottomOfDeck, (TurnTaker tt) => PlayerMovesCardResponse(tt, moved, dda), dealDamageInfo: dda.ToEnumerable(), cardSource: GetCardSource());
+                IEnumerator selectCoroutine = base.GameController.SelectTurnTakersAndDoAction(DecisionMaker, new LinqTurnTakerCriteria((TurnTaker tt) => tt.IsHero && !tt.ToHero().IsIncapacitatedOrOutOfGame && tt.ToHero().HasCardsInHand), SelectionType.MoveCardOnBottomOfDeck, (TurnTaker tt) => PlayerMovesCardResponse(tt, moved), requiredDecisions: 0, dealDamageInfo: dda.ToEnumerable(), cardSource: GetCardSource());
                 if (base.UseUnityCoroutines)
                 {
                     yield return base.GameController.StartCoroutine(selectCoroutine);
@@ -53,8 +66,15 @@ namespace VainFacadePlaytest.EldrenwoodVillage
                 {
                     base.GameController.ExhaustCoroutine(selectCoroutine);
                 }
+                foreach (MoveCardAction mca in moved)
+                {
+                    Log.Debug("TomBartonCardController.MoveCardsToPreventDamageResponse: mca.Origin: " + mca.Origin.GetFriendlyName());
+                    Log.Debug("TomBartonCardController.MoveCardsToPreventDamageResponse: mca.CardToMove: " + mca.CardToMove.Title);
+                    Log.Debug("TomBartonCardController.MoveCardsToPreventDamageResponse: mca.WasCardMoved: " + mca.WasCardMoved.ToString());
+                }
                 if (moved.Where((MoveCardAction mca) => mca.WasCardMoved).Count() >= H - 2)
                 {
+                    Log.Debug("TomBartonCardController.MoveCardsToPreventDamageResponse: preventing damage");
                     // "If {H - 2} cards are moved this way, prevent that damage."
                     IEnumerator preventCoroutine = CancelAction(dda, isPreventEffect: true);
                     if (base.UseUnityCoroutines)
@@ -67,18 +87,31 @@ namespace VainFacadePlaytest.EldrenwoodVillage
                     }
                 }
             }
+            if (IsRealAction(dda))
+            {
+                MoveCardsForDamage = null;
+            }
         }
 
         public override CustomDecisionText GetCustomDecisionText(IDecision decision)
         {
-            return new CustomDecisionText("Select a player to move a card from hand on the bottom of their deck", "choosing a player to move a card from hand on the bottom of their deck", "Vote for a player to move a card from hand on the bottom of their deck", "player to move a card from hand on the bottom of their deck");
+            return new CustomDecisionText("Do you want to move cards from hand on the bottom of their decks?", "choosing whether to move cards from hand on the bottom of their decks", "Vote for whether to move cards from hand on the bottom of their decks", "move cards from hand on the bottom of their decks");
         }
 
-        private IEnumerator PlayerMovesCardResponse(TurnTaker tt, List<MoveCardAction> moves, DealDamageAction dda)
+        private IEnumerator PlayerMovesCardResponse(TurnTaker tt, List<MoveCardAction> moves)
         {
             // "... may put a card in their hand on the bottom of their deck."
-            List<SelectCardDecision> decisions = new List<SelectCardDecision>();
-            IEnumerator selectCoroutine = base.GameController.SelectCardAndStoreResults(base.GameController.FindHeroTurnTakerController(tt.ToHero()), SelectionType.MoveCardOnBottomOfDeck, new LinqCardCriteria((Card c) => c.Location == tt.ToHero().Hand), decisions, false, gameAction: dda, cardSource: GetCardSource());
+            IEnumerator moveCoroutine = base.GameController.SelectCardFromLocationAndMoveIt(base.GameController.FindHeroTurnTakerController(tt.ToHero()), tt.ToHero().Hand, new LinqCardCriteria((Card c) => true), new MoveCardDestination[] { new MoveCardDestination(tt.Deck, toBottom: true) }, optional: true, responsibleTurnTaker: tt, storedResultsMove: moves, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(moveCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(moveCoroutine);
+            }
+            /*List<SelectCardDecision> decisions = new List<SelectCardDecision>();
+            IEnumerator selectCoroutine = base.GameController.SelectCardAndStoreResults(base.GameController.FindHeroTurnTakerController(tt.ToHero()), SelectionType.MoveCardOnBottomOfDeck, new LinqCardCriteria((Card c) => c.Location == tt.ToHero().Hand), decisions, false, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(selectCoroutine);
@@ -87,10 +120,18 @@ namespace VainFacadePlaytest.EldrenwoodVillage
             {
                 base.GameController.ExhaustCoroutine(selectCoroutine);
             }
+            foreach(SelectCardDecision scd in decisions)
+            {
+                Log.Debug("TomBartonCardController.PlayerMovesCardResponse: scd: " + scd.ToString());
+                Log.Debug("TomBartonCardController.PlayerMovesCardResponse: scd.SelectedCard: " + scd.SelectedCard.Title);
+                Log.Debug("TomBartonCardController.PlayerMovesCardResponse: scd.Completed: " + scd.Completed.ToString());
+            }
             SelectCardDecision choice = decisions.Where((SelectCardDecision scd) => scd.Completed && scd.SelectedCard != null).FirstOrDefault();
+            Log.Debug("TomBartonCardController.PlayerMovesCardResponse: choice: " + choice.ToString());
             if (choice != null)
             {
-                IEnumerator moveCoroutine = base.GameController.MoveCard(base.GameController.FindTurnTakerController(tt), choice.SelectedCard, tt.Deck, toBottom: true, responsibleTurnTaker: tt, storedResults: moves, cardSource: GetCardSource());
+                Log.Debug("TomBartonCardController.PlayerMovesCardResponse: moving " + choice.SelectedCard.Title + " to the bottom of " + tt.Deck.GetFriendlyName());
+                IEnumerator moveCoroutine = base.GameController.MoveCard(base.GameController.FindTurnTakerController(tt), choice.SelectedCard, tt.Deck, toBottom: true, decisionSources: decisions.CastEnumerable<SelectCardDecision, IDecision>(), responsibleTurnTaker: tt, storedResults: moves, cardSource: GetCardSource());
                 if (base.UseUnityCoroutines)
                 {
                     yield return base.GameController.StartCoroutine(moveCoroutine);
@@ -99,7 +140,7 @@ namespace VainFacadePlaytest.EldrenwoodVillage
                 {
                     base.GameController.ExhaustCoroutine(moveCoroutine);
                 }
-            }
+            }*/
         }
 
         public override IEnumerator ReducedToZeroResponse()
