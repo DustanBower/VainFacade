2265b4de2027f6044e4cb4368ea7c2c289dada30
kree1 <myopicmastermind@gmail.com>
2022-09-29 10:43:57 -0400

    Added Unturning Emissary, Vicious Onslaught, and Weaver's Words CardControllers

---
+++ VainFacade/TheFury/UnturningEmissaryCardController.cs
@@ -0,0 +1,71 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.TheFury
+{
+    public class UnturningEmissaryCardController : TheFuryUtilityCardController
+    {
+        public UnturningEmissaryCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            AddThisCardControllerToList(CardControllerListType.MakesIndestructible);
+        }
+
+        public override bool AskIfCardIsIndestructible(Card card)
+        {
+            // "{TheFuryCharacter} is indestructible."
+            if (card == base.CharacterCard)
+            {
+                return true;
+            }
+            return base.AskIfCardIsIndestructible(card);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "When {TheFuryCharacter} is dealt damage, discard a card or destroy this card. Then increase the next damage dealt to {TheFuryCharacter} by 1."
+            AddTrigger((DealDamageAction dda) => dda.DidDealDamage && dda.Target == base.CharacterCard, DiscardDestroyDebuffResponse, new TriggerType[] { TriggerType.DiscardCard, TriggerType.DestroySelf, TriggerType.CreateStatusEffect }, TriggerTiming.After);
+            // "At the end of your turn, if {TheFuryCharacter} has 1 or more HP, you may draw a card."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DrawCard(base.TurnTaker.ToHero()), TriggerType.DrawCard, additionalCriteria: (PhaseChangeAction pca) => base.CharacterCard.IsTarget && base.CharacterCard.HitPoints >= 1);
+            // [At the end of your turn], "If you did not play a card this turn, discard a card."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => SelectAndDiscardCards(DecisionMaker, 1, requiredDecisions: 1, responsibleTurnTaker: base.TurnTaker), TriggerType.DiscardCard, additionalCriteria: (PhaseChangeAction pca) => !base.Game.Journal.PlayCardEntriesThisTurn().Where((PlayCardJournalEntry pcje) => pcje.ResponsibleTurnTaker == base.TurnTaker).Any());
+        }
+
+        private IEnumerator DiscardDestroyDebuffResponse(DealDamageAction dda)
+        {
+            // "... discard a card or destroy this card."
+            IEnumerable<Function> options = new Function[2]
+            {
+                new Function(DecisionMaker, "Discard a card", SelectionType.DiscardCard, () => SelectAndDiscardCards(DecisionMaker, 1, requiredDecisions: 1, responsibleTurnTaker: base.TurnTaker), onlyDisplayIfTrue: base.TurnTaker.ToHero().HasCardsInHand, forcedActionMessage: base.Card.Title + " cannot be destroyed, so " + base.TurnTaker.Name + " must discard a card."),
+                new Function(DecisionMaker, "Destroy " + base.Card.Title, SelectionType.DestroySelf, () => DestroyThisCardResponse(dda), forcedActionMessage: base.TurnTaker.Name + " cannot discard any cards, so they must destroy " + base.Card.Title + ".")
+            };
+            SelectFunctionDecision choice = new SelectFunctionDecision(base.GameController, DecisionMaker, options, false, noSelectableFunctionMessage: base.TurnTaker.Name + " cannot discard any cards or destroy " + base.Card.Title + ".", cardSource: GetCardSource());
+            IEnumerator chooseCoroutine = base.GameController.SelectAndPerformFunction(choice);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(chooseCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(chooseCoroutine);
+            }
+            // "Then increase the next damage dealt to {TheFuryCharacter} by 1."
+            IEnumerator debuffCoroutine = IncreaseNextDamageTo(base.CharacterCard, 1, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(debuffCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(debuffCoroutine);
+            }
+        }
+    }
+}
---
+++ VainFacade/TheFury/WeaversWordsCardController.cs
@@ -0,0 +1,123 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.TheFury
+{
+    public class WeaversWordsCardController : TheFuryUtilityCardController
+    {
+        public WeaversWordsCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            switch(index)
+            {
+                case 0:
+                    // "1 target regains 2 HP."
+                    int numTargets = GetPowerNumeral(0, 1);
+                    int amtGained = GetPowerNumeral(1, 2);
+                    IEnumerator healCoroutine = base.GameController.SelectAndGainHP(DecisionMaker, amtGained, numberOfTargets: numTargets, requiredDecisions: numTargets, cardSource: GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(healCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(healCoroutine);
+                    }
+                    break;
+                case 1:
+                    // "Reduce the next damage dealt to {TheFuryCharacter} by 7."
+                    int amtReduce = GetPowerNumeral(0, 7);
+                    IEnumerator statusCoroutine = ReduceNextDamageTo(base.CharacterCard, amtReduce, GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(statusCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(statusCoroutine);
+                    }
+                    // "Play the top card of the villain deck."
+                    IEnumerator playCoroutine = PlayTheTopCardOfTheVillainDeckWithMessageResponse(null);
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(playCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(playCoroutine);
+                    }
+                    break;
+                case 2:
+                    // "Shuffle a trash."
+                    List<SelectLocationDecision> locationChoices = new List<SelectLocationDecision>();
+                    IEnumerator chooseCoroutine = base.GameController.SelectATrash(DecisionMaker, SelectionType.ShuffleDeck, (Location l) => base.GameController.IsLocationVisibleToSource(l, GetCardSource()), locationChoices, cardSource: GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(chooseCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(chooseCoroutine);
+                    }
+                    if (DidSelectLocation(locationChoices))
+                    {
+                        Location chosen = GetSelectedLocation(locationChoices);
+                        if (chosen != null)
+                        {
+                            IEnumerator shuffleCoroutine = base.GameController.ShuffleLocation(chosen, cardSource: GetCardSource());
+                            if (base.UseUnityCoroutines)
+                            {
+                                yield return base.GameController.StartCoroutine(shuffleCoroutine);
+                            }
+                            else
+                            {
+                                base.GameController.ExhaustCoroutine(shuffleCoroutine);
+                            }
+                            // "If the top card of that trash is not a One-Shot, play it."
+                            if (chosen.HasCards)
+                            {
+                                Card top = chosen.TopCard;
+                                if (top.IsOneShot)
+                                {
+                                    IEnumerator messageCoroutine = base.GameController.SendMessageAction("The top card of " + chosen.GetFriendlyName() + " is a One-Shot.", Priority.High, GetCardSource(), associatedCards: top.ToEnumerable());
+                                    if (base.UseUnityCoroutines)
+                                    {
+                                        yield return base.GameController.StartCoroutine(messageCoroutine);
+                                    }
+                                    else
+                                    {
+                                        base.GameController.ExhaustCoroutine(messageCoroutine);
+                                    }
+                                }
+                                else
+                                {
+                                    IEnumerator replayCoroutine = base.GameController.PlayCard(base.TurnTakerController, top, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
+                                    if (base.UseUnityCoroutines)
+                                    {
+                                        yield return base.GameController.StartCoroutine(replayCoroutine);
+                                    }
+                                    else
+                                    {
+                                        base.GameController.ExhaustCoroutine(replayCoroutine);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+            yield break;
+        }
+    }
+}
