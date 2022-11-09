fdadb54a389661ac6c25f366c74a01aa7735116f
kree1 <myopicmastermind@gmail.com>
2022-11-09 10:28:12 -0500

    Added CardControllers for Psychic Link, Resource Allocation, Reverse Psychology

---
+++ VainFacade/Node/PsychicLinkCardController.cs
@@ -0,0 +1,93 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Node
+{
+    public class PsychicLinkCardController : ConnectionCardController
+    {
+        public PsychicLinkCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // "Play with the top card of that play area's deck face up."
+            SpecialStringMaker.ShowSpecialString(() => "The top card of " + base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Deck.GetFriendlyName() + " is " + base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Deck.TopCard.Title + ".", showInEffectsList: () => base.Card.IsInPlayAndHasGameText).Condition = () => base.Card.IsInPlayAndHasGameText;
+            // If in play: show whether this card has increased damage this turn
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(DamageThisTurn, base.Card.Title + " has already increased damage this turn.", base.Card.Title + " has not increased damage this turn.").Condition = () => base.Card.IsInPlayAndHasGameText;
+        }
+
+        protected readonly string DamageThisTurn = "FirstDamageToNodeFromThisPlayAreaThisTurn";
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "The first time each turn a target in this play area would deal damage to {NodeCharacter}, increase that damage by 1."
+            AddTrigger((DealDamageAction dda) => !HasBeenSetToTrueThisTurn(DamageThisTurn) && dda.Target == base.CharacterCard && dda.DamageSource != null && dda.DamageSource.Card != null && dda.DamageSource.Card.IsTarget && dda.DamageSource.Card.Location.IsPlayAreaOf(base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker), IncreaseResponse, TriggerType.IncreaseDamage, TriggerTiming.Before);
+        }
+
+        public override IEnumerator DeterminePlayLocation(List<MoveCardDestination> destination, bool isPutIntoPlay, List<IDecision> decisionSources, Location overridePlayArea = null, LinqTurnTakerCriteria additionalTurnTakerCriteria = null)
+        {
+            // "Play this card in a non-environment play area other than {NodeCharacter}'s."
+            // Adapted from SergeantSteelTeam.MissionObjectiveCardController
+            List<SelectTurnTakerDecision> storedResults = new List<SelectTurnTakerDecision>();
+            IEnumerator coroutine = base.GameController.SelectTurnTaker(DecisionMaker, SelectionType.MoveCardToPlayArea, storedResults, optional: false, allowAutoDecide: false, (TurnTaker tt) => !tt.IsEnvironment && tt != base.TurnTaker, null, null, checkExtraTurnTakersInstead: false, canBeCancelled: true, ignoreBattleZone: false, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            SelectTurnTakerDecision selectTurnTakerDecision = storedResults.FirstOrDefault();
+            if (selectTurnTakerDecision != null && selectTurnTakerDecision.SelectedTurnTaker != null && destination != null)
+            {
+                destination.Add(new MoveCardDestination(selectTurnTakerDecision.SelectedTurnTaker.PlayArea, toBottom: false, showMessage: true));
+                yield break;
+            }
+            coroutine = base.GameController.SendMessageAction("No viable play locations. Putting this card in the trash", Priority.Low, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            destination.Add(new MoveCardDestination(base.TurnTaker.Trash, toBottom: false, showMessage: true));
+        }
+
+        private IEnumerator IncreaseResponse(DealDamageAction dda)
+        {
+            SetCardPropertyToTrueIfRealAction(DamageThisTurn, gameAction: dda);
+            // "... increase that damage by 1."
+            IEnumerator increaseCoroutine = base.GameController.IncreaseDamage(dda, 1, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(increaseCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(increaseCoroutine);
+            }
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            // "Return this card to your hand."
+            IEnumerator moveCoroutine = base.GameController.MoveCard(base.TurnTakerController, base.Card, base.TurnTaker.ToHero().Hand, showMessage: true, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(moveCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(moveCoroutine);
+            }
+        }
+    }
+}
---
+++ VainFacade/Node/ResourceAllocationCardController.cs
@@ -0,0 +1,73 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Node
+{
+    public class ResourceAllocationCardController : NodeUtilityCardController
+    {
+        public ResourceAllocationCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show list of Connected hero targets
+            SpecialStringMaker.ShowListOfCardsInPlay(new LinqCardCriteria((Card c) => IsConnected(c) && c.IsHero && c.IsTarget, "Connected hero targets", false, false, "target", "targets"));
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "When a [i]Connected[/i] hero target would deal damage, you may select another [i]Connected[/i] hero target. Increase the damage dealt by the first target by 1. Reduce the next damage dealt by the second target by 1."
+            AddTrigger((DealDamageAction dda) => dda.CanDealDamage && dda.Amount > 0 && dda.DamageSource != null && dda.DamageSource.Card != null && IsConnected(dda.DamageSource.Card) && dda.DamageSource.Card.IsHero && dda.DamageSource.Card.IsTarget, IncreaseReduceResponse, new TriggerType[] { TriggerType.WouldBeDealtDamage, TriggerType.IncreaseDamage, TriggerType.CreateStatusEffect }, TriggerTiming.Before);
+        }
+
+        private IEnumerator IncreaseReduceResponse(DealDamageAction dda)
+        {
+            // "... you may select another [i]Connected[/i] hero target."
+            Card firstTarget = dda.DamageSource.Card;
+            List<SelectCardDecision> choices = new List<SelectCardDecision>();
+            IEnumerator selectCoroutine = base.GameController.SelectCardAndStoreResults(DecisionMaker, SelectionType.SelectTargetNoDamage, new LinqCardCriteria((Card c) => IsConnected(c) && c.IsHero && c.IsTarget && c != firstTarget, "Connected hero", false, false, "target other than " + firstTarget.Title, "targets other than " + firstTarget.Title), choices, true, gameAction: dda, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(selectCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(selectCoroutine);
+            }
+            if (DidSelectCard(choices) && GetSelectedCard(choices) != null)
+            {
+                Card secondTarget = GetSelectedCard(choices);
+                // "Increase the damage dealt by the first target by 1."
+                IEnumerator increaseCoroutine = base.GameController.IncreaseDamage(dda, 1, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(increaseCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(increaseCoroutine);
+                }
+                // "Reduce the next damage dealt by the second target by 1."
+                ReduceDamageStatusEffect spent = new ReduceDamageStatusEffect(1);
+                spent.SourceCriteria.IsSpecificCard = secondTarget;
+                spent.UntilTargetLeavesPlay(secondTarget);
+                spent.NumberOfUses = 1;
+                IEnumerator statusCoroutine = base.GameController.AddStatusEffect(spent, true, GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(statusCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(statusCoroutine);
+                }
+            }
+            yield break;
+        }
+    }
+}
