415aedc263259fac5fde350ef64285d85f513c0b
kree1 <myopicmastermind@gmail.com>
2022-11-15 12:27:41 -0500

    Added CardControllers for Bleeding Edge, Network Access, and Open Line

---
+++ VainFacade/Node/BleedingEdgeCardController.cs
@@ -0,0 +1,187 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Node
+{
+    public class BleedingEdgeCardController : NodeUtilityCardController
+    {
+        public BleedingEdgeCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show list of Connected character cards in play
+            SpecialStringMaker.ShowListOfCardsInPlay(new LinqCardCriteria((Card c) => c.IsCharacter && IsConnected(c), "Connected character"));
+            // Show list of Connections in play
+            SpecialStringMaker.ShowListOfCardsInPlay(isConnection);
+        }
+
+        private bool isSelectingPlayArea = false;
+
+        public override CustomDecisionText GetCustomDecisionText(IDecision decision)
+        {
+            if (isSelectingPlayArea)
+            {
+                return new CustomDecisionText("Select a play area to play a Connection in", base.TurnTaker.Name + " is selecting a play area to play a Connection in", "Vote for a play area for " + base.TurnTaker.Name + " to play a Connection in", "play area to play a Connection in");
+            }
+            else
+            {
+                return new CustomDecisionText("Do you want to increase the next damage dealt to " + base.CharacterCard.Title + " by 1?", base.TurnTaker.Name + " is deciding whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1", "Vote for whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1", "whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1");
+            }
+        }
+
+        public override IEnumerator Play()
+        {
+            IEnumerator startCoroutine = Continue();
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(startCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(startCoroutine);
+            }
+            yield break;
+        }
+
+        public IEnumerator Continue()
+        {
+            // "You may play a Connection in a [i]Connected[/i] character's play area."
+            HeroTurnTaker player = base.TurnTaker.ToHero();
+            if (player != null && player.Hand.Cards.Where((Card c) => isConnector.Criteria(c)).Count() > 0)
+            {
+                List<Card> connectedChars = FindCardsWhere(new LinqCardCriteria((Card c) => IsConnected(c) && c.IsCharacter)).ToList();
+                List<Location> connectedLocations = (from c in connectedChars where c.Location.HighestRecursiveLocation.IsPlayArea select c.Location.HighestRecursiveLocation).Distinct().ToList();
+                if (connectedLocations.Count > 0)
+                {
+                    // Choose a Connected play area to play a Connection in
+                    List<SelectLocationDecision> choices = new List<SelectLocationDecision>();
+                    List<LocationChoice> options = new List<LocationChoice>();
+                    foreach(Location l in connectedLocations)
+                    {
+                        options.Add(new LocationChoice(l));
+                    }
+                    isSelectingPlayArea = true;
+                    IEnumerator locateCoroutine = base.GameController.SelectLocation(DecisionMaker, options, SelectionType.Custom, choices, cardSource: GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(locateCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(locateCoroutine);
+                    }
+                    if (DidSelectLocation(choices))
+                    {
+                        Location chosenDest = GetSelectedLocation(choices);
+
+                        // Choose a Connection from hand and play it in chosenDest
+                        List<Card> connectOptions = player.Hand.Cards.Where((Card c) => isConnector.Criteria(c)).ToList();
+                        IEnumerator selectConnectionHandCoroutine = base.GameController.SelectCardAndDoAction(new SelectCardDecision(base.GameController, DecisionMaker, SelectionType.PlayCard, connectOptions, cardSource: GetCardSource()), (SelectCardDecision scd) => base.GameController.PlayCard(base.TurnTakerController, scd.SelectedCard, overridePlayLocation: chosenDest, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource()));
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(selectConnectionHandCoroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(selectConnectionHandCoroutine);
+                        }
+                    }
+                }
+                else
+                {
+                    IEnumerator noLocationsCoroutine = base.GameController.SendMessageAction("There are no play areas with Connected characters to play a Connection in.", Priority.High, GetCardSource(), showCardSource: true);
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(noLocationsCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(noLocationsCoroutine);
+                    }
+                }
+            }
+            else
+            {
+                IEnumerator noConnectionsHandCoroutine = base.GameController.SendMessageAction(base.TurnTaker.Name + " has no Connection cards in their hand to play.", Priority.High, GetCardSource(), showCardSource: true);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(noConnectionsHandCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(noConnectionsHandCoroutine);
+                }
+            }
+            // "You may return any number of Connections to your hand."
+            List<MoveCardAction> returned = new List<MoveCardAction>();
+            IEnumerator returnCoroutine = base.GameController.SelectAndReturnCards(DecisionMaker, null, isConnectionInPlay, true, false, true, 0, returned, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(returnCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(returnCoroutine);
+            }
+            // "Draw a card for each Connection returned this way."
+            int numberMoved = GetNumberOfCardsMoved(returned);
+            IEnumerator drawCoroutine = DrawCards(DecisionMaker, numberMoved, false, false);
+            if (numberMoved <= 0)
+            {
+                drawCoroutine = base.GameController.SendMessageAction("No Connections were returned, so " + base.TurnTaker.Name + " does not draw any cards.", Priority.Medium, cardSource: GetCardSource(), showCardSource: true);
+            }
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(drawCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(drawCoroutine);
+            }
+            // "You may increase the next damage dealt to {NodeCharacter} by 1."
+            isSelectingPlayArea = false;
+            YesNoDecision damageChoice = new YesNoDecision(base.GameController, DecisionMaker, SelectionType.Custom, associatedCards: base.CharacterCard.ToEnumerable(), cardSource: GetCardSource());
+            IEnumerator increaseChoiceCoroutine = base.GameController.MakeDecisionAction(damageChoice);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(increaseChoiceCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(increaseChoiceCoroutine);
+            }
+            if (DidPlayerAnswerYes(damageChoice))
+            {
+                IncreaseDamageStatusEffect vulnerability = new IncreaseDamageStatusEffect(1);
+                vulnerability.TargetCriteria.IsSpecificCard = base.CharacterCard;
+                vulnerability.UntilTargetLeavesPlay(base.CharacterCard);
+                vulnerability.NumberOfUses = 1;
+                IEnumerator statusCoroutine = base.GameController.AddStatusEffect(vulnerability, true, GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(statusCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(statusCoroutine);
+                }
+                // "If you do, repeat the entire game text of this card."
+                IEnumerator repeatCoroutine = Continue();
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(repeatCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(repeatCoroutine);
+                }
+            }
+            yield break;
+        }
+    }
+}
---
+++ VainFacade/Node/OpenLineCardController.cs
@@ -0,0 +1,202 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VainFacadePlaytest.Node
+{
+    public class OpenLineCardController : ConnectionCardController
+    {
+        public OpenLineCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        private bool ReplacingActionByNode = false;
+        private bool ActionToReplaceIsDraw = false;
+
+        public override CustomDecisionText GetCustomDecisionText(IDecision decision)
+        {
+            string otherPlayers = "";
+            string repeated = "whether ";
+            if (ReplacingActionByNode)
+            {
+                otherPlayers += base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name;
+                repeated += base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name;
+            }
+            else
+            {
+                otherPlayers += base.TurnTaker.Name;
+                repeated += base.TurnTaker.Name;
+            }
+            string q = "Do you want to ";
+            otherPlayers += " is deciding whether to ";
+            repeated += " should ";
+            if (ActionToReplaceIsDraw)
+            {
+                q += "draw";
+                otherPlayers += "draw";
+                repeated += "draw";
+            }
+            else
+            {
+                q += "play";
+                otherPlayers += "play";
+                repeated += "play";
+            }
+            q += " a card instead of ";
+            repeated += " a card instead of ";
+            otherPlayers += " a card instead of ";
+            if (ReplacingActionByNode)
+            {
+                q += base.TurnTaker.Name + "?";
+                otherPlayers += base.TurnTaker.Name;
+                repeated += base.TurnTaker.Name;
+            }
+            else
+            {
+                q += base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name + "?";
+                otherPlayers += base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name;
+                repeated += base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.Name;
+            }
+            string votingPlayers = "Vote for " + repeated;
+
+            return new CustomDecisionText(q, otherPlayers, votingPlayers, repeated);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "When you or that play area's owner would draw or play a card, the other may do so instead."
+            AddTrigger((DrawCardAction dca) => base.TurnTaker.IsHero && base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.IsHero && (dca.HeroTurnTaker == base.TurnTaker.ToHero() || dca.HeroTurnTaker == base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.ToHero()), OfferDrawExchangeResponse, new TriggerType[] { TriggerType.CancelAction, TriggerType.DrawCard }, TriggerTiming.Before);
+            AddTrigger((PlayCardAction pca) => base.TurnTaker.IsHero && base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.IsHero && (pca.ResponsibleTurnTaker == base.TurnTaker || pca.ResponsibleTurnTaker == base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker), OfferPlayExchangeResponse, new TriggerType[] { TriggerType.CancelAction, TriggerType.PlayCard }, TriggerTiming.Before);
+        }
+
+        private IEnumerator OfferDrawExchangeResponse(DrawCardAction dca)
+        {
+            ActionToReplaceIsDraw = true;
+            HeroTurnTaker currentlyDrawing = dca.HeroTurnTaker;
+            ReplacingActionByNode = false;
+            HeroTurnTaker couldDrawInstead = base.TurnTaker.ToHero();
+            if (currentlyDrawing == base.TurnTaker.ToHero())
+            {
+                ReplacingActionByNode = true;
+                couldDrawInstead = base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.ToHero();
+            }
+            YesNoDecision choice = new YesNoDecision(base.GameController, FindHeroTurnTakerController(couldDrawInstead), SelectionType.Custom, cardSource: GetCardSource());
+            IEnumerator chooseCoroutine = base.GameController.MakeDecisionAction(choice);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(chooseCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(chooseCoroutine);
+            }
+            if (DidPlayerAnswerYes(choice))
+            {
+                IEnumerator cancelCoroutine = CancelAction(dca);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(cancelCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(cancelCoroutine);
+                }
+                IEnumerator drawCoroutine = DrawCard(couldDrawInstead);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(drawCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(drawCoroutine);
+                }
+            }
+        }
+
+        private IEnumerator OfferPlayExchangeResponse(PlayCardAction pca)
+        {
+            ActionToReplaceIsDraw = false;
+            HeroTurnTaker currentlyPlaying = pca.ResponsibleTurnTaker.ToHero();
+            ReplacingActionByNode = false;
+            HeroTurnTaker couldPlayInstead = base.TurnTaker.ToHero();
+            if (currentlyPlaying == base.TurnTaker.ToHero())
+            {
+                ReplacingActionByNode = true;
+                couldPlayInstead = base.Card.Location.HighestRecursiveLocation.OwnerTurnTaker.ToHero();
+            }
+            YesNoDecision choice = new YesNoDecision(base.GameController, FindHeroTurnTakerController(couldPlayInstead), SelectionType.Custom, cardSource: GetCardSource());
+            IEnumerator chooseCoroutine = base.GameController.MakeDecisionAction(choice);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(chooseCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(chooseCoroutine);
+            }
+            if (DidPlayerAnswerYes(choice))
+            {
+                IEnumerator cancelCoroutine = CancelAction(pca);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(cancelCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(cancelCoroutine);
+                }
+                IEnumerator playCoroutine = SelectAndPlayCardFromHand(base.GameController.FindHeroTurnTakerController(couldPlayInstead), optional: false, associateCardSource: true);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(playCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(playCoroutine);
+                }
+            }
+            yield break;
+        }
+
+        public override IEnumerator DeterminePlayLocation(List<MoveCardDestination> destination, bool isPutIntoPlay, List<IDecision> decisionSources, Location overridePlayArea = null, LinqTurnTakerCriteria additionalTurnTakerCriteria = null)
+        {
+            // "Play this card in another hero's play area."
+            // Adapted from SergeantSteelTeam.MissionObjectiveCardController
+            List<SelectTurnTakerDecision> storedResults = new List<SelectTurnTakerDecision>();
+            IEnumerator coroutine = base.GameController.SelectTurnTaker(DecisionMaker, SelectionType.MoveCardToPlayArea, storedResults, optional: false, allowAutoDecide: false, (TurnTaker tt) => tt.IsHero && tt != base.TurnTaker, null, null, checkExtraTurnTakersInstead: false, canBeCancelled: true, ignoreBattleZone: false, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            SelectTurnTakerDecision selectTurnTakerDecision = storedResults.FirstOrDefault();
+            if (selectTurnTakerDecision != null && selectTurnTakerDecision.SelectedTurnTaker != null && destination != null)
+            {
+                destination.Add(new MoveCardDestination(selectTurnTakerDecision.SelectedTurnTaker.PlayArea, toBottom: false, showMessage: true));
+                yield break;
+            }
+            coroutine = base.GameController.SendMessageAction("No viable play locations. Putting this card in the trash", Priority.Low, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            destination.Add(new MoveCardDestination(base.TurnTaker.Trash, toBottom: false, showMessage: true));
+        }
+    }
+}
