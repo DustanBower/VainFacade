9f589bec128bd3f6fcfaa410ba3bc7f160da381a
kree1 <myopicmastermind@gmail.com>
2022-11-16 16:22:43 -0500

    Fixed The I in Team's start phase effect not counting as a self-destruct for Psychological Fortitude

--- VainFacade/Node/TheIInTeamCardController.cs
+++ VainFacade/Node/TheIInTeamCardController.cs
@@ -22,9 +22,36 @@ namespace VainFacadePlaytest.Node
         {
             base.AddTriggers();
             // "At the start of your turn, {NodeCharacter} may deal herself 3 irreducible psychic damage. If she takes no damage this way, destroy this card."
-            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction p) => DealDamageOrDestroyThisCardResponse(p, base.CharacterCard, base.CharacterCard, 3, DamageType.Psychic, true), new TriggerType[] { TriggerType.DealDamage, TriggerType.DestroySelf });
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction p) => DealDamageOrDestroySelfResponse(p, base.CharacterCard, base.CharacterCard, 3, DamageType.Psychic, true), new TriggerType[] { TriggerType.DealDamage, TriggerType.DestroySelf });
             // "When a [i]Connected[/i] hero target would be dealt damage, you may redirect that damage to another [i]Connected[/i] hero target. Reduce damage redirected this way by 1."
             AddTrigger((DealDamageAction dda) => dda.CanDealDamage && dda.IsRedirectable && IsConnected(dda.Target) && dda.Target.IsHero, RedirectReduceResponse, new TriggerType[] { TriggerType.RedirectDamage, TriggerType.ReduceDamage }, TriggerTiming.Before);
+        }
+
+        private IEnumerator DealDamageOrDestroySelfResponse(GameAction gameAction, Card damageSource, Card target, int amount, DamageType damageType, bool isIrreducible = false)
+        {
+            List<DealDamageAction> storedResults = new List<DealDamageAction>();
+            Card thisCard = Card;
+            IEnumerator coroutine = DealDamage(damageSource, target, amount, damageType, isIrreducible, optional: true, isCounterDamage: false, null, storedResults);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            if (Card == thisCard && !DidDealDamage(storedResults, target))
+            {
+                IEnumerator coroutine2 = GameController.DestroyCard(DecisionMaker, Card, optional: false, responsibleCard: base.Card, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine2);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine2);
+                }
+            }
         }
 
         private IEnumerator RedirectReduceResponse(DealDamageAction dda)
