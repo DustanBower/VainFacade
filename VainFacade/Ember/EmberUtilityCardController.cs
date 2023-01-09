164f0e84666475fd5ab1aa15f806f216462140dd
kree1 <myopicmastermind@gmail.com>
2023-01-09 12:13:56 -0500

    Added From the Ashes, Scorching Shriek, and Searing Talons CardControllers

---
+++ VainFacade/Ember/EmberUtilityCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Ember
+{
+    public class EmberUtilityCardController : CardController
+    {
+        public EmberUtilityCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        protected static readonly string BlazeKeyword = "blaze";
+
+        protected static LinqCardCriteria BlazeCard = new LinqCardCriteria((Card c) => c.DoKeywordsContain(BlazeKeyword), "Blaze");
+        protected static LinqCardCriteria BlazeCardInPlay = new LinqCardCriteria((Card c) => c.DoKeywordsContain(BlazeKeyword) && c.IsInPlayAndHasGameText, "Blaze", singular: "card in play", plural: "cards in play");
+
+        protected int NumBlazeCardsInPlay()
+        {
+            return base.GameController.FindCardsWhere(BlazeCardInPlay, visibleToCard: GetCardSource()).Count();
+        }
+    }
+}
