f2eec04a254b69d926b16bdf50c2ecd9460992b8
kree1 <myopicmastermind@gmail.com>
2023-01-24 12:50:37 -0500

    Added Fired Up CardController and Blitz UtilityCardController

---
+++ VainFacade/Blitz/BlitzUtilityCardController.cs
@@ -0,0 +1,25 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Blitz
+{
+    public class BlitzUtilityCardController : CardController
+    {
+        public BlitzUtilityCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        protected const string CircuitKeyword = "circuit";
+        protected const string PlaybookKeyword = "playbook";
+
+        public LinqCardCriteria IsCircuit = new LinqCardCriteria((Card c) => c.DoKeywordsContain(CircuitKeyword), "Circuit");
+    }
+}
