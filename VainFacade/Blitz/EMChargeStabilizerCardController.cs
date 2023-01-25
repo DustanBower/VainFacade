a52e132619680e4030df6361142f70b7f9c7507b
kree1 <myopicmastermind@gmail.com>
2023-01-25 13:12:04 -0500

    Added EM-Charge Stabilizer and Power Source CardControllers

---
+++ VainFacade/Blitz/EMChargeStabilizerCardController.cs
@@ -0,0 +1,32 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VainFacadePlaytest.Blitz
+{
+    public class EMChargeStabilizerCardController : CardController
+    {
+        public EMChargeStabilizerCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show hero target with highest HP
+            SpecialStringMaker.ShowHeroTargetWithHighestHP();
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "Increase lightning damage dealt by 1."
+            AddIncreaseDamageTrigger((DealDamageAction dda) => dda.DamageType == DamageType.Lightning, (DealDamageAction dda) => 1);
+            // "When {BlitzCharacter} is dealt lightning damage, he regains 3 HP."
+            AddTrigger((DealDamageAction dda) => dda.Target == base.CharacterCard && dda.DamageType == DamageType.Lightning && dda.DidDealDamage, (DealDamageAction dda) => base.GameController.GainHP(base.CharacterCard, 3, cardSource: GetCardSource()), TriggerType.GainHP, TriggerTiming.After);
+            // "At the end of the villain turn, {BlitzCharacter} deals the hero target with the highest HP 1 lightning damage."
+            AddDealDamageAtEndOfTurnTrigger(base.TurnTaker, base.CharacterCard, (Card c) => c.IsHero && c.IsTarget, TargetType.HighestHP, 1, DamageType.Lightning);
+        }
+    }
+}
