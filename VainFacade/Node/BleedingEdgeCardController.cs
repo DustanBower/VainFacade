933eb49fba9006582be7fe3997297fa4332096c4
kree1 <myopicmastermind@gmail.com>
2023-02-27 12:47:46 -0500

    Updated Bleeding Edge card text and effect

--- VainFacade/Node/BleedingEdgeCardController.cs
+++ VainFacade/Node/BleedingEdgeCardController.cs
@@ -14,125 +14,25 @@ namespace VainFacadePlaytest.Node
         public BleedingEdgeCardController(Card card, TurnTakerController turnTakerController)
             : base(card, turnTakerController)
         {
-            // Show list of Connected character cards in play
-            SpecialStringMaker.ShowListOfCardsInPlay(new LinqCardCriteria((Card c) => c.IsCharacter && IsConnected(c), "Connected character"));
             // Show list of Connections in play
             SpecialStringMaker.ShowListOfCardsInPlay(isConnection);
         }
 
-        private bool isSelectingPlayArea = false;
-
-        public override CustomDecisionText GetCustomDecisionText(IDecision decision)
-        {
-            if (isSelectingPlayArea)
-            {
-                return new CustomDecisionText("Select a play area to play a Connection in", base.TurnTaker.Name + " is selecting a play area to play a Connection in", "Vote for a play area for " + base.TurnTaker.Name + " to play a Connection in", "play area to play a Connection in");
-            }
-            else
-            {
-                return new CustomDecisionText("Do you want to increase the next damage dealt to " + base.CharacterCard.Title + " by 1?", base.TurnTaker.Name + " is deciding whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1", "Vote for whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1", "whether to increase the next damage dealt to " + base.CharacterCard.Title + " by 1");
-            }
-        }
-
         public override IEnumerator Play()
         {
-            IEnumerator startCoroutine = Continue();
+            // "You may play a card."
+            IEnumerator freePlayCoroutine = SelectAndPlayCardFromHand(DecisionMaker, associateCardSource: true);
             if (base.UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(startCoroutine);
+                yield return base.GameController.StartCoroutine(freePlayCoroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(startCoroutine);
+                base.GameController.ExhaustCoroutine(freePlayCoroutine);
             }
-        }
-
-        public IEnumerator Continue()
-        {
-            // "You may play a Connection in a [i]Connected[/i] character's play area."
-            HeroTurnTaker player = base.TurnTaker.ToHero();
-            if (player != null && player.Hand.Cards.Where((Card c) => isConnector.Criteria(c)).Count() > 0)
-            {
-                List<Card> connectedChars = FindCardsWhere(new LinqCardCriteria((Card c) => IsConnected(c) && c.IsCharacter)).ToList();
-                List<Location> connectedLocations = (from c in connectedChars where c.Location.HighestRecursiveLocation.IsPlayArea select c.Location.HighestRecursiveLocation).Distinct().ToList();
-                List<Card> connectOptions = player.Hand.Cards.Where((Card c) => isConnector.Criteria(c)).ToList();
-                if (connectedLocations.Count > 0)
-                {
-                    // Choose a Connected play area to play a Connection in
-                    List<SelectTurnTakerDecision> choices = new List<SelectTurnTakerDecision>();
-                    List<TurnTaker> options = new List<TurnTaker>();
-                    foreach(Location l in connectedLocations)
-                    {
-                        bool hasValidConnection = false;
-                        foreach(Card c in connectOptions)
-                        {
-                            if (FindCardController(c) is ConnectionCardController && (FindCardController(c) as ConnectionCardController).IsValidPlayArea(l.OwnerTurnTaker))
-                            {
-                                hasValidConnection = true;
-                                break;
-                            }
-                        }
-                        if (hasValidConnection)
-                        {
-                            options.Add(l.OwnerTurnTaker);
-                        }
-                    }
-                    isSelectingPlayArea = true;
-                    IEnumerator locateCoroutine = base.GameController.SelectTurnTaker(DecisionMaker, SelectionType.Custom, choices, optional: true, additionalCriteria: (TurnTaker tt) => options.Contains(tt), cardSource: GetCardSource());
-                    if (base.UseUnityCoroutines)
-                    {
-                        yield return base.GameController.StartCoroutine(locateCoroutine);
-                    }
-                    else
-                    {
-                        base.GameController.ExhaustCoroutine(locateCoroutine);
-                    }
-
-                    if (DidSelectTurnTaker(choices))
-                    {
-                        Location chosenDest = GetSelectedTurnTaker(choices).PlayArea;
-
-                        // Choose a Connection from hand and play it in chosenDest
-                        List<Card> restrictedOptions = connectOptions.Where((Card c) => FindCardController(c) is ConnectionCardController && (FindCardController(c) as ConnectionCardController).IsValidPlayArea(GetSelectedTurnTaker(choices))).ToList();
-                        IEnumerator selectConnectionHandCoroutine = base.GameController.SelectCardAndDoAction(new SelectCardDecision(base.GameController, DecisionMaker, SelectionType.PlayCard, restrictedOptions, cardSource: GetCardSource()), (SelectCardDecision scd) => base.GameController.PlayCard(base.TurnTakerController, scd.SelectedCard, overridePlayLocation: chosenDest, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource()));
-                        if (base.UseUnityCoroutines)
-                        {
-                            yield return base.GameController.StartCoroutine(selectConnectionHandCoroutine);
-                        }
-                        else
-                        {
-                            base.GameController.ExhaustCoroutine(selectConnectionHandCoroutine);
-                        }
-                    }
-                }
-                else
-                {
-                    IEnumerator noLocationsCoroutine = base.GameController.SendMessageAction("There are no play areas with Connected characters to play a Connection in.", Priority.High, GetCardSource(), showCardSource: true);
-                    if (base.UseUnityCoroutines)
-                    {
-                        yield return base.GameController.StartCoroutine(noLocationsCoroutine);
-                    }
-                    else
-                    {
-                        base.GameController.ExhaustCoroutine(noLocationsCoroutine);
-                    }
-                }
-            }
-            else
-            {
-                IEnumerator noConnectionsHandCoroutine = base.GameController.SendMessageAction(base.TurnTaker.Name + " has no Connection cards in their hand to play.", Priority.High, GetCardSource(), showCardSource: true);
-                if (base.UseUnityCoroutines)
-                {
-                    yield return base.GameController.StartCoroutine(noConnectionsHandCoroutine);
-                }
-                else
-                {
-                    base.GameController.ExhaustCoroutine(noConnectionsHandCoroutine);
-                }
-            }
-            // "You may return any number of Connections to your hand."
-            List<MoveCardAction> returned = new List<MoveCardAction>();
-            IEnumerator returnCoroutine = base.GameController.SelectAndReturnCards(DecisionMaker, null, isConnectionInPlay, true, false, false, 0, returned, cardSource: GetCardSource());
+            // "Return any number of Connections in play to your hand."
+            List<MoveCardAction> storedMoves = new List<MoveCardAction>();
+            IEnumerator returnCoroutine = base.GameController.SelectAndReturnCards(DecisionMaker, null, isConnection, true, false, false, 0, storedMoves, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(returnCoroutine);
@@ -141,57 +41,50 @@ namespace VainFacadePlaytest.Node
             {
                 base.GameController.ExhaustCoroutine(returnCoroutine);
             }
-            // "Draw a card for each Connection returned this way."
-            int numberMoved = GetNumberOfCardsMoved(returned);
-            IEnumerator drawCoroutine = DrawCards(DecisionMaker, numberMoved, false, false);
-            if (numberMoved <= 0)
+            // "For each Connection returned this way, you may draw a card, then you may play a card."
+            int numberMoved = GetNumberOfCardsMoved(storedMoves);
+            for (int i = 0; i < numberMoved; i++)
             {
-                drawCoroutine = base.GameController.SendMessageAction("No Connections were returned, so " + base.TurnTaker.Name + " does not draw any cards.", Priority.Medium, cardSource: GetCardSource(), showCardSource: true);
-            }
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(drawCoroutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(drawCoroutine);
-            }
-            // "You may increase the next damage dealt to {NodeCharacter} by 1."
-            isSelectingPlayArea = false;
-            YesNoDecision damageChoice = new YesNoDecision(base.GameController, DecisionMaker, SelectionType.Custom, associatedCards: base.CharacterCard.ToEnumerable(), cardSource: GetCardSource());
-            IEnumerator increaseChoiceCoroutine = base.GameController.MakeDecisionAction(damageChoice);
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(increaseChoiceCoroutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(increaseChoiceCoroutine);
-            }
-            if (DidPlayerAnswerYes(damageChoice))
-            {
-                IncreaseDamageStatusEffect vulnerability = new IncreaseDamageStatusEffect(1);
-                vulnerability.TargetCriteria.IsSpecificCard = base.CharacterCard;
-                vulnerability.UntilTargetLeavesPlay(base.CharacterCard);
-                vulnerability.NumberOfUses = 1;
-                IEnumerator statusCoroutine = base.GameController.AddStatusEffect(vulnerability, true, GetCardSource());
+                IEnumerator drawCoroutine = DrawCard(base.HeroTurnTaker, optional: true);
                 if (base.UseUnityCoroutines)
                 {
-                    yield return base.GameController.StartCoroutine(statusCoroutine);
+                    yield return base.GameController.StartCoroutine(drawCoroutine);
                 }
                 else
                 {
-                    base.GameController.ExhaustCoroutine(statusCoroutine);
+                    base.GameController.ExhaustCoroutine(drawCoroutine);
                 }
-                // "If you do, repeat the entire game text of this card."
-                IEnumerator repeatCoroutine = Continue();
+                IEnumerator playCoroutine = SelectAndPlayCardFromHand(DecisionMaker, associateCardSource: true);
                 if (base.UseUnityCoroutines)
                 {
-                    yield return base.GameController.StartCoroutine(repeatCoroutine);
+                    yield return base.GameController.StartCoroutine(playCoroutine);
                 }
                 else
                 {
-                    base.GameController.ExhaustCoroutine(repeatCoroutine);
+                    base.GameController.ExhaustCoroutine(playCoroutine);
+                }
+            }
+            // "Increase the next damage dealt to {NodeCharacter} by X, where X is the number of hero cards played since this card entered play."
+            PlayCardJournalEntry thisWasPlayed = base.GameController.Game.Journal.QueryJournalEntries((PlayCardJournalEntry e) => e.CardPlayed == base.Card).LastOrDefault();
+            if (thisWasPlayed != null)
+            {
+                int? thisIndex = base.GameController.Game.Journal.GetEntryIndex(thisWasPlayed);
+                if (thisIndex.HasValue)
+                {
+                    int numberHeroCardsPlayedSince = (from pcje in base.Journal.PlayCardEntries() where pcje.CardPlayed.IsHero && base.GameController.Game.Journal.GetEntryIndex(pcje).HasValue && base.GameController.Game.Journal.GetEntryIndex(pcje).Value > thisIndex.Value select pcje).Count();
+                    IncreaseDamageStatusEffect vulnerability = new IncreaseDamageStatusEffect(numberHeroCardsPlayedSince);
+                    vulnerability.TargetCriteria.IsSpecificCard = base.CharacterCard;
+                    vulnerability.UntilTargetLeavesPlay(base.CharacterCard);
+                    vulnerability.NumberOfUses = 1;
+                    IEnumerator statusCoroutine = base.GameController.AddStatusEffect(vulnerability, true, GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(statusCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(statusCoroutine);
+                    }
                 }
             }
         }
