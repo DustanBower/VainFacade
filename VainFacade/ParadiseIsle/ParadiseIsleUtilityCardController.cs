7ecc6f2c5dea73a722ac5c22853586a7bcdcf599
kree1 <myopicmastermind@gmail.com>
2023-03-02 15:18:19 -0500

    Improved SpecialString phrasing for No, Mr. Rooke, I Expect You to Die

--- VainFacade/ParadiseIsle/ParadiseIsleUtilityCardController.cs
+++ VainFacade/ParadiseIsle/ParadiseIsleUtilityCardController.cs
@@ -19,7 +19,7 @@ namespace VainFacadePlaytest.ParadiseIsle
 
         public static readonly string ConspiratorKeyword = "conspirator";
 
-        public static LinqCardCriteria isConspirator = new LinqCardCriteria((Card c) => c.DoKeywordsContain(ConspiratorKeyword), "Conspirator");
+        public static LinqCardCriteria isConspirator = new LinqCardCriteria((Card c) => c.DoKeywordsContain(ConspiratorKeyword), "", false, false, "Conspirator", "Conspirators");
         public static LinqCardCriteria isConspiratorInPlay = new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain(ConspiratorKeyword), "Conspirator in play", false, false, "Conspirator in play", "Conspirators in play");
     }
 }
