a94605d89a0ac68eae8eb703d58fab6f1aff34ef
origamiswami <76876227+origamiswami@users.noreply.github.com>
2024-01-19 18:17:23 -0500

    Add files via upload

---
+++ VainFacade/Glyph/Might/SuntulusCompendiumCardController.cs
@@ -0,0 +1,19 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VainFacadePlaytest.Glyph.Might
+{
+	public class SuntulusCompendiumCardController:CardController
+	{
+		public SuntulusCompendiumCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+		}
+	}
+}
+
