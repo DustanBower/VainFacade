ed9fccecf0f5d1ed95b8e40f9e04d5aa034338fd
origamiswami <76876227+origamiswami@users.noreply.github.com>
2025-06-09 18:25:50 -0400

    Allotted Time Mode Select
    Add Diving Save-style mode selection for Allotted Time

--- VainFacade/TheFury/AllottedTimeCardController.cs
+++ VainFacade/TheFury/AllottedTimeCardController.cs
@@ -20,13 +20,73 @@ namespace VainFacadePlaytest.TheFury
         private Card ProtectedCard { get; set; }
         private Guid ReactedDamage { get; set; }
 
+        private string BasicMode = "AllottedTimeBasicMode";
+        private string ModeSelected = "AllotedTimeModeSelected";
+
+        public override IEnumerator Play()
+        {
+            if (!IsPropertyCurrentlyTrue(ModeSelected))
+            {
+                IEnumerator coroutine = SetModeResponse();
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+        }
+
         public override void AddTriggers()
         {
             base.AddTriggers();
             // "When a target would be dealt damage, you may make that target indestructible this turn."
-            AddTrigger((DealDamageAction dda) => !dda.IsPretend && dda.IsSuccessful && dda.CanDealDamage && dda.Amount > 0, MayProtectResponse, TriggerType.CreateStatusEffect, TriggerTiming.Before);
+            AddTrigger<DealDamageAction>(AllottedTimeCriteria, MayProtectResponse, TriggerType.CreateStatusEffect, TriggerTiming.Before);
             // "Resolve that damage normally, then that target regains X HP and destroys this card, where X = 1 plus the damage dealt this way."
             AddTrigger((DealDamageAction dda) => !dda.IsPretend && dda.InstanceIdentifier == ReactedDamage, HealDestructResponse, new TriggerType[] { TriggerType.GainHP, TriggerType.DestroySelf }, TriggerTiming.After);
+
+            //Ask what mode to use at the start of Fury's turn
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == this.TurnTaker && !IsPropertyTrue(ModeSelected), (PhaseChangeAction pca) => SetModeResponse(), TriggerType.Hidden);
+            AddAfterLeavesPlayAction(() => ResetFlagAfterLeavesPlay(BasicMode));
+            AddAfterLeavesPlayAction(() => ResetFlagAfterLeavesPlay(ModeSelected));
+        }
+
+        //Based on Diving Save
+        private IEnumerator SetModeResponse()
+        {
+            SelectFunctionDecision selectControlMode = new SelectFunctionDecision(functionChoices: new List<Function>
+            {
+                new Function(base.HeroTurnTakerController, $"Basic Control: Only allow {this.Card.Title} to make hero targets indestructible", SelectionType.None, () => SetFlags(flag: true)),
+                new Function(base.HeroTurnTakerController, $"Full Control: Allow {this.Card.Title} to make any target indestructible", SelectionType.None, () => SetFlags(flag: false))
+            }, gameController: base.GameController, hero: base.HeroTurnTakerController, optional: false, gameAction: null, noSelectableFunctionMessage: null, associatedCards: null, cardSource: GetCardSource());
+            IEnumerator performFunction = base.GameController.SelectAndPerformFunction(selectControlMode, null, new Card[1] { base.Card });
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(performFunction);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(performFunction);
+            }
+        }
+
+        //Based on Diving Save
+        private IEnumerator SetFlags(bool flag)
+        {
+            IEnumerable<Card> GotHim = base.TurnTaker.GetCardsByIdentifier(this.Card.Identifier);
+            foreach (Card ds in GotHim)
+            {
+                base.GameController.FindCardController(ds).SetCardProperty(BasicMode, flag);
+                base.GameController.FindCardController(ds).SetCardProperty(ModeSelected, value: true);
+            }
+            yield return null;
+        }
+
+        private bool AllottedTimeCriteria(DealDamageAction dda)
+        {
+            return !dda.IsPretend && dda.IsSuccessful && dda.CanDealDamage && dda.Amount > 0 && (IsHeroTarget(dda.Target) || !IsPropertyCurrentlyTrue(BasicMode));
         }
 
         public override CustomDecisionText GetCustomDecisionText(IDecision decision)
